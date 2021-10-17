cf43dec11265e94e64ec9880aa7fba4b1e831e2f
Abdul <arifb@uwindsor.ca>
2021-10-17 01:16:05 -0400

    deployed blog using Heroku!
    used node.js, heroku, postgres SQL

--- abdul-arif-blog/routes/root.js
+++ abdul-arif-blog/routes/root.js
@@ -2,6 +2,16 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return reply.view('./templates/index.hbs', {name: "Abdul's Awesome"})
+    return reply.view('./templates/index.hbs', { name: 'Abdul' })
+  })
+
+  fastify.get('/about', async function (request, reply) {
+    const { rows } = await fastify.pg.query('SELECT * FROM users LIMIT 1')
+    const [user] = rows
+    return reply.view('./templates/about.hbs', { user })
+  })
+
+  fastify.get('/contact', async function (request, reply) {
+    return reply.view('./templates/contact.hbs', { name: 'Abdul' })
   })
 }
