7e3cd501e217485db72bd2248205049228b0c024
Abdul Arif <arifb@uwindsor.ca>
2021-10-18 00:06:43 -0400

    Update

--- abdul-arif-blog/routes/posts/index.js
+++ abdul-arif-blog/routes/posts/index.js
@@ -4,7 +4,7 @@ const marked = require('marked')
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    let { rows: posts } = await fastify.pg.query(
+    let { rows: posts } = await fastify.pg.query( // returns the posts from the blog
       'SELECT title, description, content FROM posts ORDER BY created_at DESC LIMIT 10'
     )
 
@@ -17,12 +17,12 @@ module.exports = async function (fastify, opts) {
     return reply.view('./templates/posts.hbs', { posts })
   })
 
-  fastify.get('/new', async function (request, reply) {
+  fastify.get('/new', async function (request, reply) { // allows users to create new posts
     return reply.view('./templates/new.hbs')
   })
 
-  fastify.post('/new', async function (request, reply) {
-    const { title, description, content } = request.body
+  fastify.post('/new', async function (request, reply) { // .post saves the information into the database
+    const { title, description, content } = request.body // gets data from request.body
     const { rows } = await fastify.pg.query('SELECT id FROM users LIMIT 1')
     const [user] = rows
 
